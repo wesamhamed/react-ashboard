195deb668e1772fa231b8db2db30bced9618d835
wesam hamed <w.m.h.h2022@gmail.com>
2021-09-16 17:50:05 +0300

    first dashboard using react

--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './reportWebVitals'
+
+import { createStore } from 'redux'
+
+import { Provider } from 'react-redux'
+
+import rootReducer from './redux/reducers'
+
+import './assets/boxicons-2.0.7/css/boxicons.min.css'
+import './assets/css/grid.css'
+import './assets/css/theme.css'
+import './assets/css/index.css'
+
+import Layout from './components/layout/Layout'
+
+const store = createStore(
+  rootReducer
+)
+
+document.title = 'Tua CRM'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <Layout />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
